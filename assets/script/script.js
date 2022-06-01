1187c97d564596b547010eb1d6d0733942900159
Tyler <Tyl3r.s@outlook.com>
2022-06-01 16:23:38 -0400

    finalize HTML and CSS

---
+++ assets/script/script.js
@@ -0,0 +1,39 @@
+var question1 = [
+    
+
+
+]
+
+
+var timer = function(){
+
+};
+
+var start = function(event){
+    event.preventDefault();
+    
+};
+
+var quizQuestions = function(){
+
+};
+
+var checkAnswers = function (){
+
+};
+
+var clearAnswers = function (){
+
+};
+
+var wrongAnswer = function (){
+
+};
+
+var correctAnswer = function (){
+
+};
+
+var highScores = function (){
+
+};
