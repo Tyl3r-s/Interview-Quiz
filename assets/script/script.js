622e6e1716b132567c9f7362d2fdaa2cda203a57
Tyler <Tyl3r.s@outlook.com>
2022-06-03 11:57:55 -0400

    quiz end added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,36 @@
-// array containing all of the questions
+// First question and answer array
 let questionOne = {
     question: "Commonly used data types do NOT include:",
     answers: ["strings", "booleans", "alerts", "numbers"],
     correctAnswer: 1
 }
+// Second question and answer array
+let questionTwo = {
+    question: "The condition in an if/else statement is enclosed with _____.",
+    answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    correctAnswer: 2
+}
+// Third question and answer array
+let questionThree = {
+    question: "Arrays in JavaScript are used to store _____.",
+    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswer: 3
+}
 
+// which question car you're on
+var i = 0;
+// total score variable
 var score = 0;
+
+// timer variable
+var counter = 3;
+
+// quiz box element
+var questionContainer = document.getElementById("container");
+
+var answerButtonsEl = document.getElementById("answer-buttons")
+
+var questionEl = document.getElementById("insertQ")
 
 // start button function
 var startButton = document.getElementById("start-btn");
@@ -13,25 +38,89 @@ var startButton = document.getElementById("start-btn");
 startButton.addEventListener("click", startGame);
 
 function startGame() {
-
+    count = 50;
     startButton.classList.add("hide");
     questionContainer.classList.remove("hide");
     document.getElementById("count-box").style="color:green;";
     startTimer();
-    showQuestion(questionOne);
+    showQuestionThree();
 };
 
+
 // load question after start click
-function showQuestion(q) {
-    // 1 select dom element
+function showQuestionOne() {
+    // show question
     let questionDiv = document.getElementById("insertQ");
-    // 2 modify element
-    questionDiv.textContent = q.question;
-    // selecting by query
+    questionDiv.textContent = questionOne.question;
+
+    // show answers
     let qAnswers = document.querySelectorAll(".answer");
+    
     qAnswers.forEach(function(element, index){
-        element.textContent = q.answers[index];
-    }); 
+        element.textContent = questionOne.answers[index];
+
+        element.addEventListener('click', function(){
+            // check for correct
+            if  (questionOne.correctAnswer == index) {
+                console.log("correct");
+                score++
+            }
+            else {
+                console.log("wrong");
+                counter = counter - 5;
+            } 
+        })
+    });
+}
+
+function showQuestionTwo() {
+    // show question
+    let questionDiv = document.getElementById("insertQ");
+    questionDiv.textContent = questionTwo.question;
+
+    // show answers
+    let qAnswers = document.querySelectorAll(".answer");
+    
+    qAnswers.forEach(function(element, index){
+        element.textContent = questionTwo.answers[index];
+
+        element.addEventListener('click', function(){
+            // check for correct
+            if  (questionTwo.correctAnswer == index) {
+                console.log("correct");
+                score++
+            }
+            else {
+                console.log("wrong");
+                counter = counter - 5;
+            } 
+        })
+    });
+}
+
+function showQuestionThree() {
+    // show question
+    let questionDiv = document.getElementById("insertQ");
+    questionDiv.textContent = questionThree.question;
+
+    // show answers
+    let qAnswers = document.querySelectorAll(".answer");
+    
+    qAnswers.forEach(function(element, index){
+        element.textContent = questionThree.answers[index];
+
+        element.addEventListener('click', function(){
+            // check for correct
+            if  (questionThree.correctAnswer == index) {
+                console.log("correct");
+                score++
+            }
+            else {
+                console.log("wrong");
+                counter = counter - 5;
+            } 
+        })
+    });
 }
 
 // quiz timer 
@@ -43,149 +132,15 @@ function startTimer() {
             span = document.getElementById("count-box");
             span.innerHTML = counter;
         }
-        else if (counter === 0) {
+        if (counter === 0) {
             quizEnd();
             clearInterval(counter);
         }
     }, 1000);
 };
 
-
-// var questions = [
-//     {question: "Commonly used data types do NOT include:"},
-//     {question: "The condition in an if/else statement is enclosed with _____."},
-//     {question: "Arrays in JavaScript are used to store _____."},
-//     {question: "String values must be enclosed within _____ when being assigned to variables."}, 
-//     {question: "A very useful tool used during development and debugging for printing content to the debugger is:"}
-// ];
-
-// array containing every answer, including if they are correct or not
-// var answers = [
-//     {text: "strings", correct: false},
-//     {text: "booleans", correct: true},
-//     {text: "alerts", correct: false},
-//     {text: "numbers", correct: false},
-
-//     {text: "quotes", correct: false },
-//     {text: "curly brackets", correct: false},
-//     {text: "parenthesis", correct: true},
-//     {text: "square brackets", correct: false},
-
-//     {text: "numbers and strings", correct: false },
-//     {text: "other arrays", correct: false},
-//     {text: "booleans", correct: false},
-//     {text: "all of the above", correct: true},
-    
-//     {text: "commas", correct: false },
-//     {text: "curly brackets", correct: true},
-//     {text: "quotes", correct: false},
-//     {text: "parenthesis", correct: false},
-
-//     {text: "javascript", correct: false },
-//     {text: "terminal/bash", correct: false},
-//     {text: "for loops", correct: true},
-//     {text: "console.log", correct: false}
-// ];
-
-// Dom Elements
-var pageContentEl = document.querySelector("#page-content");
-
-
-
-var questionContainer = document.getElementById("container");
-
-var time = document.getElementById("count-box");
-
-var answerButtonsEl = document.querySelector("#answer-buttons");
-
-var questionEl = document.getElementById("insertQ");
-
-var answerEl = document.getElementById("ansr-btn");
-
-var ansrOneEl = document.querySelector("#ansr-btn1");
-
-var ansrTwoEl = document.querySelector("#ansr-btn2");
-
-var ansrThreeEl = document.querySelector("#ansr-btn3");
-
-var ansrFourEl = document.querySelector("#ansr-btn4");
-
-// Variables
-var ansr1 = 0
-
-var ansr2 = 1
-
-var ansr3 = 2
-
-var ansr4 = 3
-
-var counter = 50;
-
-var score = 0;
-
-var startQuiz = 0;
-
-var questionCount = 0;
-
-var correct = 0;
-
-var answerCount = 0;
-
-// Load the next question function
-// function loadQuestion() {
-//     if (startQuiz < 5) {
-//         questionEl.textContent=questions[questionCount].question
-//         answerQuestion()
-//         loadAnswers()
-//     }
-//     else {
-//        quizEnd()
-//     }
-
-//     questionCount++;
-//     startQuiz++;
-//     answerCount++;
-//     console.log(questionCount, startQuiz, answerCount);
-// };
-
-// // Load the next answer function
-// function loadAnswers() {
-//     ansrOneEl.textContent=answers[ansr1].text;
-//     ansrTwoEl.textContent=answers[ansr2].text;
-//     ansrThreeEl.textContent=answers[ansr3].text;
-//     ansrFourEl.textContent=answers[ansr4].text;
-// };
-
-// // Answer click function
-// function answerQuestion() {
-//     ansrOneEl.addEventListener("click", loadQuestion);
-//     ansrTwoEl.addEventListener("click", loadQuestion);
-//     ansrThreeEl.addEventListener("click", loadQuestion);
-//     ansrFourEl.addEventListener("click", loadQuestion);
-//     if (startQuiz > 0) {
-//         ansr1 = ansr1 + 4;
-//         ansr2 = ansr2 + 4;
-//         ansr3 = ansr3 + 4;
-//         ansr4 = ansr4 + 4;
-//       }
-// };
-
-// // if(startQuiz === 0) {
-// //     document.getElementById("ansr-btn2").onclick = function(){
-// //         console.log("correct");
-// //     }
-// // };
-
-// // The end of quiz 
-// function quizEnd() {
-//     answerButtonsEl.classList.add("hide");
-//     questionEl.textContent="Finish!"
-//     counter = 0;
-//     questionCount = 0;
-//     startQuiz = 0;
-//     answerCount = 0;
-//     ansr1 = 0;
-//     ansr2 = 1;
-//     ansr3 = 2;
-//     ansr4 = 3;
-// };
+// The end of quiz 
+function quizEnd() {
+    answerButtonsEl.classList.add("hide");
+    questionEl.textContent="Finish!";
+};
